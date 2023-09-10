22fbc5f5a9b37abbac01c8140284c5787b703bba
Unknown <137830287+KGF-420@users.noreply.github.com>
2023-09-10 17:45:09 +0500

    Update KGF.py

--- KGF.py
+++ KGF.py
@@ -1,6 +1,5 @@
 import os, sys, platform,time
 bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('clear')
+if bit == '64bit'
     os.system('git pull')
-    import off
+    import kgf
